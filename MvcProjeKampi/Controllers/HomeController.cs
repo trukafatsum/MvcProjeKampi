4e0d1007fb072777d6a25fc40d766d816d64aafd
trukafatsum <trukafatsum@DESKTOP-LFHMI03>
2023-02-13 17:04:16 +0300

    Add project files.

---
+++ MvcProjeKampi/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() //Index metodu //View tarafında karşılığı var
+        {
+            return View();
+        }
+
+        public ActionResult About() //Hakkında metodu //View tarafında karşılığı var
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() //İletişim metodu //View tarafında karşılığı var
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test ()
+        {
+            return View();
+        }
+    }
+}
