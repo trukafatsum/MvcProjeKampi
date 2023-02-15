2afa3b0a440e374d63cf7340f98f41b2740182e8
Mustafa KURT <trukafatsum@gmail.com>
2023-02-15 13:09:09 +0300

    Görev 2/4 Ders 40

---
+++ MvcProjeKampi/Controllers/IstatistikController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer.Concrete;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        // GET: Istatistik
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            var result = context.Categories.Count();
+            var result2 = context.Headings.Count(x => x.Category.CategoryID == 10);
+            var result3 = context.Writers.Count(x => x.WriterName.Contains("a"));
+            var result4 = context.Headings.Max(x => x.Category.CategoryName);
+            var result5 = context.Categories.Count(x => x.CategoryStatus == true);
+            var result6 = context.Categories.Count(x => x.CategoryStatus == false);
+            ViewBag.CategoryCount = result;
+            ViewBag.Heading = result2;
+            ViewBag.Writer = result3;
+            ViewBag.HeadingMax = result4;
+            ViewBag.StatusGap = (result5 - result6);
+
+            return View();
+        }
+    }
+}
